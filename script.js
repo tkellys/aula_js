37f352b7de1c1f9c5124256f535cd896a6df8000
tkellys <116043363+tkellys@users.noreply.github.com>
2023-01-02 21:54:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// const saladaFrutas = ["banana","maçã","goiaba","manga","morango"];
+
+// console.log("salada 0: ",saladaFrutas);
+
+// saladaFrutas.push("leite condensado");
+// console.log("salada 1: ", saladaFrutas);
+
+// saladaFrutas.push("leite em pó");
+// console.log("salada 2: ",saladaFrutas);
+
+// saladaFrutas.pop();
+// console.log("salada 3: ",saladaFrutas);
+
+// const itemRemovido = saladaFrutas.pop();
+// console.log("Item removido: ", itemRemovido);
+
+// const nomes = ["Ronier", "Larissa", "Vandecir", "Davi", "Alessandro"]
+
+// let posicao = nomes.indexOf("Davi");
+
+
+// if(posicao === -1){
+//     console.log("Nome não encontrado");
+    
+// }else {
+// console.log(nomes[posicao]);
+
+// }
+
+// const saladaFrutas = ["banana","maçã","goiaba","manga","morango"];
+// function capsLk(valor) {
+//     return valor.toUpperCase();
+// }
+// const saladaUpper = saladaFrutas.map((fruta) => fruta.toUpperCase());
+// console.log(saladaFrutas);
+// console.log(saladaUpper);
+
+const nomes = ["Ronier", "Larissa", "Vandecir", "Davi", "Alessandro"]
+
+function filtrarNomes(nome) {
+    if(nome === "Davi");
+    return nome;
+}  
+console.log(nome.filter(filtrarNomes));
